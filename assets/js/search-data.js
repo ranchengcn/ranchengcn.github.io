0f5185857d941fd57ff4c68b30cc098778e8f274
ranchengcn <ranchengcn@gmail.com>
2026-08-18 01:21:12 +0000

    Deploying to gh-pages from @ ranchengcn/ranchengcn.github.io@09571ba4d7dad0e392c74e455643bf4a263b0f4a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -69,8 +69,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_5/";
-            },},{id: "news-we-released-evox-genesis-an-ai-system-for-long-horizon-autonomous-software-evolution",
-          title: 'We released EvoX Genesis: an AI system for long-horizon autonomous software evolution',
+            },},{id: "news-we-released-evox-genesis",
+          title: 'We released EvoX Genesis',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_6/";
